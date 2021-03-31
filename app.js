4e582d3ed57f6c22d6402b708e19108852506ccf
David Carpenter <davidcarpenter510@gmail.com>
2021-03-31 12:21:07 -0600

    fixing mongoose connection

--- app.js
+++ app.js
@@ -41,9 +41,15 @@ app.use((req, res, next) => {
 const main = async () => {
     await mongoose.connect(`${process.env.DGM4790_CONNECTION_STRING}`,
     {
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
+    try {
+        await mongoose.connect(`${process.env.DGM4790_CONNECTION_STRING}`, 
+        { useNewUrlParser: true})
+    } catch (error) {
+        handleError(error)
+    }
     app.listen(port, () => {
         console.log('Testing app listening at http://localhost:${port}')
     })
